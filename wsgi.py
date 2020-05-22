9c43c85cb2f8a6c2241b19af0c2168e384bcdfe9
Ron <ronjohn4@gmail>
2020-05-21 20:44:35 -0600

    prep for release

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,3 @@
-# todo - Create Test Harness
-# Load on Start
-# Web based page
-# Display all Keyvals return
-# Allow Instance to be set in UI
-# Reload button
-# Example of use of one keyval in code
-
 # todo - enforce data types in the UI
 # todo - make sure all names are unique within their bag
 # todo - add logging
@@ -17,6 +9,4 @@ from app import app
 
 
 if __name__ == "__main__":
-    # bag.jinja_env.auto_reload = True
     app.run(port=5000, debug=True)
-    # bag.run(host='0.0.0.0')
